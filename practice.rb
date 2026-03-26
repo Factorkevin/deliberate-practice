46e8f234a9549377968300ffb1521e1c1a6eec21
Factorkevin <factorkevin24@gmail.com>
2026-03-26 15:12:13 +1000

    Add exercise solutions

--- practice.rb
+++ practice.rb
@@ -4,3 +4,18 @@ candy = 20
 gum = 15
  puts "sum of sweets:", 20 + 15
 
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+Name = "kevin"
+puts Name.upcase
+
+#Write a program that uses variables to store two numbers, then prints the numbers multiplied together.
+
+x =10
+y=8
+puts x * y 
+
+#Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+question = "do you know who I am?"
+puts question.reverse 
